23b138d8e642e44811357830b0ec3ea59e03749f
palunel <33368866+palunel@users.noreply.github.com>
2019-03-22 22:08:06 +0200

    Delete tempCodeRunnerFile.py

--- tempCodeRunnerFile.py
+++
@@ -1,4 +0,0 @@
-    features = [0, 1, 2, 6, 7, 8, 13, 14]
-    combined_data = Combine(files)
-    combined_data = Clean(combined_data, remove_nan=True, indices=features)
-    np.savetxt('combined_data.txt', combined_data)
