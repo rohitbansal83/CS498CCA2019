157980fbe10283053c3c276a5e8965d2758c1310
Paul Nel <gpaul.nel@gmail.com>
2019-04-30 14:46:37 +0200

    updated repo structure

--- outliers.R
+++ outliers.R
@@ -3,7 +3,7 @@ library(MASS)
 
 # Unproccessed Cleveland Data
 colNames <- c("age", "sex", "cp", "trestbps", "chol","fbs", "restecg", "thalach", "exang","oldpeak", "slope", "ca", "thal", "num")
-data <- data.frame(read.table("pre_cleveland.txt", col.names = colNames, header=FALSE, sep =" "))
+data <- data.frame(read.table("data/pre_cleveland.txt", col.names = colNames, header=FALSE, sep =" "))
 cleveland.lm <- lm(num ~ age + sex + cp + trestbps + chol + fbs + restecg + thalach + exang + oldpeak + slope + ca + thal, data=data)
 plot(cleveland.lm, which=5, id.n = 297)
 abline(2,0, lty=4, col = 59 )
@@ -11,7 +11,7 @@ abline(-2,0, lty=4, col = 59 )
 summary(cleveland.lm)
 
 # Proccessed Cleveland Data
-data <- data.frame(read.table("cleveland_data.txt", col.names = colNames, header=FALSE, sep =" "))
+data <- data.frame(read.table("data/cleveland_data.txt", col.names = colNames, header=FALSE, sep =" "))
 cleveland.lm <- lm(num ~ age + sex + cp + trestbps + chol + fbs + restecg + thalach + exang + oldpeak + slope + ca + thal, data=data)
 plot(cleveland.lm, which=5, id.n = 290)
 abline(2,0, lty=4, col = 59 )
@@ -20,7 +20,7 @@ summary(cleveland.lm)
 
 # Unproccessed Combined Data
 colNames <- c("age", "sex", "cp","restecg", "thalach", "exang", "set_id", "num")
-data <- data.frame(read.table("pre_combined.txt", col.names = colNames, header=FALSE, sep =" "))
+data <- data.frame(read.table("data/pre_combined.txt", col.names = colNames, header=FALSE, sep =" "))
 combined <- lm(num ~ age + sex + cp + restecg + thalach + exang + set_id, data=data)
 plot(combined, which=5, id.n = 20)
 abline(2,0, lty=4, col = 59 )
@@ -28,7 +28,7 @@ abline(-2,0, lty=4, col = 59 )
 summary(combined)
 
 # Proccessed Combined Data
-data <- data.frame(read.table("combined_data.txt", col.names = colNames, header=FALSE, sep =" "))
+data <- data.frame(read.table("data/combined_data.txt", col.names = colNames, header=FALSE, sep =" "))
 combined <- lm(num ~ age + sex + cp + restecg + thalach + exang + set_id, data=data)
 plot(combined, which=5, id.n = 20)
 abline(2,0, lty=4, col = 59 )
